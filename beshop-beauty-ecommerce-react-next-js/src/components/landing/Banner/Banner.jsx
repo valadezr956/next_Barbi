31c268333c8ea1449d816279dc8f0dc05b7f22b2
valadezr956 <valadezr956@gmail.com>
2023-02-22 14:25:28 -0600

    init commit

---
+++ beshop-beauty-ecommerce-react-next-js/src/components/landing/Banner/Banner.jsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+
+export const Banner = () => {
+  return (
+    <>
+      {/* <!-- BEGIN MAIN BLOCK --> */}
+      <div className='main-block load-bg'>
+        <div className='wrapper'>
+          <div className='main-block__content'>
+            <span className='saint-text'>Professional</span>
+            <h1 className='main-text'>Barbi Beauty Studio</h1>
+            <p>
+              Ultrasound cavitation and Radio frequency
+            </p>
+
+            <Link href='/shop'>
+              <a className='btn'>Book Now</a>
+            </Link>
+          </div>
+        </div>
+        <img
+          className='main-block__decor'
+          src='/assets/img/main-block-decor.png'
+          alt=''
+        />
+      </div>
+      {/* <!-- MAIN BLOCK EOF --> */}
+    </>
+  );
+};
